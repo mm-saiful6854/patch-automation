19d57541fdc7b74379c1ee3f5f72cd28b1c5fcc5
Md Saiful Islam <md.saiful@revesoft.com>
2023-04-08 11:15:18 +0600

    phase 2

--- project.py
+++ project.py
@@ -1,24 +1,46 @@
 import os
 import re
 import paramiko
+import datetime,time
+import sys
+import getpass
 
+
+'''
+Rules:
+1. Python method name follows the snake_case convention. 
+This convention makes method names easy to read and understand, especially for longer method names.
+
+'''
 
 # constant
+system='windows'
 basic_config_filename = 'basic_config.txt'
 remote_host_config_filename='remote_host_config_filename'
-local_file_path='local_file_path'
-remote_dir_path='remote_dir_path'
+local_file_path_param='local_file_path'
+last_check_time_param='last_check_time'
+time_string_format='%d-%m-%Y %H:%M:%S'
+
 
 remote_host_param_name ='host'
 remote_port_param_name ='port'
 remote_username_param_name ='username'
 remote_password_param_name ='password'
+remote_dir_path_param_name='remote_dir_path'
 # end constant
 
 
 
 
-
+def detect_system():
+    global system
+    # sys.platform.startswith('darwin') is required for mac-os. this os is out of support till now.
+    if sys.platform.startswith('win'):
+        system ='windows'
+    elif sys.platform.startswith('linux'):
+        system='linux'
+    else:
+        system='unknown'
 
 
 """
@@ -28,8 +50,19 @@ remote_password_param_name ='password'
     # Define the remote destination directory for the file
     remote_dir_path = '/path/to/remote/directory/'
 """
-def uploadFile(local_file_path, remote_dir_path, host, port, username, password):
+def upload_file(local_file_path, remote_dir_path, host, port, username, password, patch_list):
     
+    print("Uploading on this host: ")
+    print("\t'{}': '{}'".format('local file path', local_file_path))
+    print("\t'{}': '{}'".format('host', host))
+    print("\t'{}': '{}'".format('port', port))
+    print("\t'{}': '{}'".format('username', username))
+    print("\t'{}': '{}'".format('password', password))
+    print("\t'{}': '{}'".format('remote dir path', remote_dir_path))
+    
+
+
+
     is_upload=True
     try:
         # Create an SSH client instance
@@ -45,21 +78,9 @@ def uploadFile(local_file_path, remote_dir_path, host, port, username, password)
         # Create an SCP client instance
         scp_client = ssh_client.open_sftp()
 
-        # Upload the local file to the remote host
-        scp_client.put(local_file_path, remote_dir_path)
-
-
-        # Unzip the file on the remote host
-        stdin, stdout, stderr = ssh_client.exec_command('unzip '+remote_dir_path+' -d /usr/local/SMS_APP_SERVER_v4Dev/')
-
-        # Print the output of the command
-        for line in stdout:
-            print(line.strip())
-
-        for line in stdin:
-            print(line.strip())
-
-
+        for filename in patch_list:
+            # Upload the local file to the remote host
+            scp_client.put(local_file_path+filename, remote_dir_path+filename)
 
     except Exception as err:
         is_upload = False 
@@ -71,6 +92,23 @@ def uploadFile(local_file_path, remote_dir_path, host, port, username, password)
     return is_upload
 
 
+
+'''incompleted'''
+def execute_command(ssh_client):
+    # Unzip the file on the remote host
+    remote_dir_path=""
+    stdin, stdout, stderr = ssh_client.exec_command('unzip '+remote_dir_path+' -d /usr/local/SMS_APP_SERVER_v4Dev/')
+
+    # Print the output of the command
+    for line in stdout:
+        print(line.strip())
+
+    for line in stdin:
+        print(line.strip())
+
+
+
+'''incompleted'''
 def execute_command_by_invoke_shell(ssh_client):
     # start an interactive shell
     shell = ssh_client.invoke_shell()
@@ -101,7 +139,12 @@ parameter=value
 return value is dictionary like this:
 {parameter: value}
 '''
-def readfile(filename):
+def read_file(filename):
+    if(filename==None):
+        return
+    
+
+    mydict={}
     # Open the file for reading
     with open(filename, "r") as file:
         # Read the contents of the file into a variable
@@ -109,22 +152,21 @@ def readfile(filename):
         # Close the file
         file.close()
         # start manipulation
-        config_dictionary={}
         lines = fullcontents.splitlines();
         for line in lines:
             if(not line.startswith('#')):
                 value = line.split('=')
                 if(len(value)==2):
-                    config_dictionary[value[0]] = value[1]
+                    mydict[value[0]] = value[1]
 
         # end manipulation    
-    return config_dictionary
+    return mydict
 
 
 
 
 
-def getListOfCredentials(host_dict):
+def get_list_of_host_details(host_dict):
     # Sort the dictionary by its keys
     sorted_host_dic = dict(sorted(host_dict.items()))
 
@@ -135,24 +177,78 @@ def getListOfCredentials(host_dict):
     remote_dir=[]
 
     for key, value in host_dict.items():
-        if(key.startswith('host')):
+        if(key.startswith(remote_host_param_name)):
             ip.append(value)
 
-        if(key.startswith('port')):
+        if(key.startswith(remote_port_param_name)):
             port.append(value)
 
-        if(key.startswith('username')):
+        if(key.startswith(remote_username_param_name)):
             username.append(value)
     
-        if(key.startswith('password')):
+        if(key.startswith(remote_password_param_name)):
             password.append(value)
-        if(key.startswith('remote_dir_path')):
+        if(key.startswith(remote_dir_path_param_name)):
             remote_dir.append(value)
     
             
     return (ip, port, username, password, remote_dir)
 
+def convert_time_string_into_unix_time(time_string):
+    if(len(time_string)==0):
+        return 
+    try:
+        dt_obj = datetime.datetime.strptime(time_string, time_string_format)
+        # multipy by 1000 in order to adding last millisecond
+        unix_time = int(dt_obj.timestamp()*1000)
+        return unix_time
+    except Exception as err:
+        print(err)
+    return
 
+
+
+def get_updated_file_list(source_dir_path, last_check_time_str, current_time):
+    print("\nchecking for patch: ")
+    
+    last_check_time = convert_time_string_into_unix_time(last_check_time_str)
+    current_unix_time = convert_time_string_into_unix_time(current_time)
+    print('\tcurrent time: {}'.format(current_unix_time))
+    print("\tlast check unix time: {}".format(last_check_time))
+    print('')
+
+    patch_file_list=[]
+    files = os.listdir(source_dir_path)
+    for file in files:
+        # Get the full path of the file
+        file_path = os.path.join(source_dir_path, file)
+        # Get the file's details
+        file_stats = os.stat(file_path)
+        # type casting float -> int
+        last_modified = int(file_stats.st_mtime*1000)
+
+        # Print the file's details
+        print(f'\tFilename: {file}')
+        print(f'\tLast modified: {last_modified}')
+        if(last_modified>last_check_time and last_modified<=current_unix_time):
+            patch_file_list.append(file)
+        """print(f'Path: {file_path}')
+        print(f'Size: {file_stats.st_size} bytes')
+        print(f'Last accessed: {file_stats.st_atime}')
+        print(f'Created: {file_stats.st_ctime}')"""
+        print('')
+    return patch_file_list
+
+
+'''save current time into basic_config.txt file as last check time'''
+def save_last_check_time(filename,mydict):
+    # open file in write mode
+    with open(filename, 'w') as f:
+        # write data to the file
+        for key, value in mydict.items():
+            f.write(key+'='+ str(value)+'\n')
+
+'''validate ip using regular expression'''
 def is_valid_ip_address(ip_address):
     ip_pattern = r"^(\d{1,3}\.){3}\d{1,3}$"
     if re.match(ip_pattern, ip_address):
@@ -162,73 +258,109 @@ def is_valid_ip_address(ip_address):
     
 
 
-def display(my_dict):
+'''print into console in a decent manner for increasing system readability'''
+def display_mydict(my_dict):
     for key, value in my_dict.items():
-        print("\t'{}': '{}'".format(key, value))
+        if(key.startswith("password")):
+            print("\t'{}': '{}'".format(key, '*************'))
+        else:
+            print("\t'{}': '{}'".format(key, value))
 
 
 def main():
-    print("Program version v1.0.0")
-    print("paramiko lib version: {}".format(paramiko.__version__))
-    
-    # load basic configuration
-    basicConfig= readfile(filename=basic_config_filename)
-    print("# Basic Configuration:")
-    display(basicConfig)
+    print("============================ Program v1.0.0 ==============================")
+    current_time_str = datetime.datetime.now().strftime(time_string_format)
+    print("Up Time:", current_time_str)
 
+    # detect the running operating system. If operating system is unknown, then exit
+    detect_system()
+    if(system=='unknown'):
+        print("Support unavailable. Sorry")
+        return
+    
+    print("Detected Os: {}".format(system))
+    print("paramiko package version: {}".format(paramiko.__version__))
+    print('')
+
+    username = getpass.getuser()
+    print(f"Hello {username}. Hope everything is fine. I am patch-automation bot.\nHere I am for helping you to push patch from your local computer to your remote servers\n")
+    
+    
     # next target
     '''
         individual remote dir path - done
-        feature of append new host interactively.
+        feature of append new host interactively. - pause
         prompt password for each host, otherwise store encrypted password 
-        check file modification time: upload those files that are modify later
+        check file modification time: upload those files that are modify later - done
+        write current time as last check time into basic_config.txt file - done
+        parse directory
 
     '''
 
 
-    # get dictionary of remote host
-    host_dic = readfile(filename=basicConfig.get(remote_host_config_filename))
+    # load basic configuration
+    basicConfig= read_file(filename=basic_config_filename)
+    print("# Basic Configuration:")
+    display_mydict(basicConfig)
+
+    if(basicConfig == None):
+        print("Invalid Basic Configuration. exiting...")
+        return
+
+
+    # get dictionary of remote host configuration
+    host_dict = read_file(filename=basicConfig.get(remote_host_config_filename))
     print("# Host Configuration:")
-    display(host_dic)
+    display_mydict(host_dict)
+    if(host_dict == None):
+        print("Invalid Host Configuration. exiting...")
+        return
 
     # get list of host, port, username, password
-    ips, ports, usernames, passwords, remote_dirs = getListOfCredentials(host_dict = host_dic)
+    ips, ports, usernames, passwords, remote_dirs = get_list_of_host_details(host_dict = host_dict)
 
 
 
-    # uploading 
-    source_file_path = basicConfig.get(local_file_path)
+    # fetching patch files
+    source_dir_path = basicConfig.get(local_file_path_param)
+    last_check_time_str = basicConfig.get(last_check_time_param)
 
-
-    for itr in range(len(ips)):
-        host = ips[itr]
-        port = ports[itr]
-        user = usernames[itr]
-        password = passwords[itr]
-        remote_dir = remote_dirs[itr]
-        
-        if(is_valid_ip_address(ip_address=host)):
-            status = uploadFile(local_file_path=source_file_path, 
-                       remote_dir_path=remote_dir,
-                       host=host,
-                       port=port,
-                       username=user,
-                       password=password)
-            if(status==True):
-                print("File uploaded successfully!")
-            else:
-                print("failed to upload")
+    patch_file_list = get_updated_file_list(source_dir_path, last_check_time_str, current_time_str)
+    print("Patch file List: {}".format(patch_file_list))
     
-    # end uploading 
+    if(len(patch_file_list)==0):
+        print("No need to push anything.")
+    else:
+        for itr in range(len(ips)):
+            host = ips[itr]
+            port = ports[itr]
+            user = usernames[itr]
+            password = passwords[itr]
+            remote_dir = remote_dirs[itr]
+        
+            if(is_valid_ip_address(ip_address=host)):
+                status = upload_file(local_file_path=source_dir_path, remote_dir_path=remote_dir,
+                host=host,
+                port=port,
+                username=user,
+                password=password, patch_list=patch_file_list)
+                if(status==True):
+                    print("File upload done!")
+                else:
+                    print("failed to upload")
+                
+        # end uploading 
 
-
+    print("Store last check time into file")
+    basicConfig[last_check_time_param] = current_time_str
+    save_last_check_time(basic_config_filename,basicConfig)
+    print("Everything goes fine.\nSee you again...")
 
 
     
 
 if __name__ == '__main__':
-    print("start program")
     main()
-    print("end program successfully")
+    print("End")
 
 
--- test_project.py
+++ test_project.py
@@ -1,13 +1,43 @@
+import project
+import pytest
 
 
 
 
 
-def test_readfile():
-    pass
+"""def test_read_file():
+    pass"""
 
-def test_getListOfCredentials():
-    pass
 
-def test_is_valid_ip_address():
-    pass
+"""@pytest.mark.parametrize("mydict, expected", [
+    (1, 2, 3),
+    (0, 0, 0),
+    (-1, 1, 0),
+    (2, -2, 0)
+])
+def test_get_list_of_host_details(host_dict, expected):
+    assert project.get_list_of_host_details(host_dict) == expected"""
+
+'''
+    perform unit test on this method is_valid_ip_address
+'''
+@pytest.mark.parametrize("ip, expected", [
+    ("192.2.1.0",True),
+    ("02-02-2023 00:00:00",False)
+
+])
+def test_is_valid_ip_address(ip, expected):
+    assert project.is_valid_ip_address(ip) == expected
+
+
+
+'''
+    perform unit test on this method convert_time_string_into_unix_time
+'''
+@pytest.mark.parametrize("time_string, expected", [
+    ("05-04-2023 10:00:00",1680667200000),
+    ("02-02-2023 00:00:00",1675274400000)
+
+])
+def test_convert_time_string_into_unix_time(time_string, expected):
+    assert project.convert_time_string_into_unix_time(time_string) == expected
